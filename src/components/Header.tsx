6286d0bac05f942659a89ea71ad1ea3d6baf584c
Haric0tmer <laurent.boualavong@hotmail.fr>
2024-05-13 18:06:04 +0200

    Header with buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom';
-
+import { Button } from './ui/button';
 function Header() {
    return (
-      <nav>
-         <Link to={'/projects'}>Projet</Link>
-         <Link to={'/aboutme'}>A propos</Link>
-         <Link to={'/contact'}>Contact</Link>
+      <nav className="flex m-2 justify-between items-center border h-[90px]">
+         <img src="/bdh" alt="logo" />
+         <div className="flex justify-between w-[350px]">
+            <Button variant={'ghost'}>
+               <Link to={'/'}>Homepage</Link>
+            </Button>
+            <Button variant={'ghost'}>
+               <Link to={'/contact'}>Contact</Link>
+            </Button>
+            <Button variant={'ghost'}>
+               <Link to={'/projects'}>Projet</Link>
+            </Button>
+            <Button variant={'secondary'} className="bg-slate-300">
+               <Link to={'/aboutme'}>À propos</Link>
+            </Button>
+         </div>
       </nav>
    );
 }
