c7e6a5904818b06396538ec01f162dfc0b6a28c8
Haric0tmer <laurent.boualavong@hotmail.fr>
2024-07-01 16:59:19 +0200

    ajout de hopefield

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,11 +2,25 @@ interface ProjectProps {
    locale: string;
 }
 
+import ProjectCard from './ProjectCard';
+
 function Projects({ locale }: ProjectProps) {
    return locale === 'fr' ? (
-      <div id="projects">Projets</div>
+      <div id="projects">
+         <h2>Mes projets</h2>
+         <p>La faudra mettre un peu de blabla</p>
+         <div className="flex flex-col justify-center items-center">
+            <ProjectCard locale={locale}></ProjectCard>
+         </div>
+      </div>
    ) : (
-      <div id="projects">Projects</div>
+      <div id="projects">
+         <h2>My projects</h2>
+         <p>La faudra mettre un peu de blabla</p>
+         <div className="flex flex-col justify-center items-center">
+            <ProjectCard locale={locale}></ProjectCard>
+         </div>
+      </div>
    );
 }
 
