c81988a1faac1a151c68d27e974f18e6567a21d9
Haric0tmer <laurent.boualavong@hotmail.fr>
2025-02-17 17:36:35 +0100

    Corrected spelling mistakes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -27,7 +27,7 @@ function Timeline({ locale }: TimelineProps) {
 							color='yellow'
 							title='La prepa des INP'
 							date='Sept 2020 - Juil 2021'
-							desc="Après le lycée, j'ai fait le choix d'intégrer la prépa des INP de Nancy dans le but de rejoindre une école d'ingénieur du groupe INP. Durant cette année, j'ai pu découvrir plusieurs domaine scientifiques en profondeur malgrès des conditions de travail compliquées par le confinement du COVID-19. J'ai notamment pu découvrir l'informatique ce qui m'a poussé vers ma décision d'arrêter la prépa pour m'orienter vers des études d'informatique."
+							desc="Après le lycée, j'ai fait le choix d'intégrer la prépa des INP de Nancy dans le but de rejoindre une école d'ingénieur du groupe INP. Durant cette année, j'ai pu découvrir plusieurs domaines scientifiques en profondeur malgré des conditions de travail compliquées par le confinement du COVID-19. J'ai notamment pu découvrir l'informatique ce qui m'a décidé d'arrêter la prépa en fin d'anné pour m'orienter vers des études d'informatique."
 							type='school'
 						></TimelineElement>
 					) : (
@@ -36,7 +36,7 @@ function Timeline({ locale }: TimelineProps) {
 							title='La prepa des INP'
 							date='Sept 2020 - Jul 2021'
 							type='school'
-							desc='After high school, I decided to join La prepa des INP in Nancy with the goal of joining an engineering school from the INP group. During this year, I was able to discover several scientific fields in depth despite difficult working conditions due to the COVID-19 lockdown. I was able to discover computer science which pushed me towards my decision to stop my current formation and go towards computer science studies.'
+							desc='After high school, I decided to join La prepa des INP in Nancy with the goal of joining an engineering school from the INP group. During this year, I was able to discover several scientific fields in depth despite difficult working conditions due to the COVID-19 lockdown. I was able to discover computer science which convinced me to go towards computer science studies for the next year.'
 						></TimelineElement>
 					)}
 				</div>
@@ -70,7 +70,7 @@ function Timeline({ locale }: TimelineProps) {
 							color='red'
 							title='Stage - DATE'
 							date='Avr 2023 - Juil 2023'
-							desc="Pendant ma deuxième année de BUT, j'ai eu a faire un stage d'une durée de 3 mois ayant pour but de donner un avant-goût du monde du travail. Pour ce stage j'ai travaillé chez DATE, une PME ayant pour coeur de métier la fabrication d'échangeur thermique à forte téchnicité, faits sur mesure pou des clients du domaine du spatial, de la défense ou encore de l'industrie. Ma mission chez DATE était de réaliser une application permettant de chiffrer un projet en prennant en compte les différents postes de dépenses allant de la main d'oeuvre à la matière première. J'ai pu découvrir le monde de l'industrie et de l'entreprise, mais aussi apprendre à travailler en autonomie et à gérer un projet de A à Z."
+							desc="Pendant ma deuxième année de BUT, j'ai fait un stage de 3 mois pour comprendre le monde du travail. Pour ce stage j'ai travaillé chez DATE, une PME ayant pour coeur de métier la fabrication d'échangeur thermique à forte technicité, faits sur mesure pour des clients du domaine spatial, de la défense ou encore de l'industrie. Ma mission chez DATE était de réaliser une application permettant de chiffrer un projet en prenant en compte les différents postes de dépenses allant de la main d'oeuvre à la matière première. J'ai pu découvrir le monde de l'industrie et de l'entreprise, mais aussi apprendre à travailler en autonomie et à gérer un projet du début à la fin."
 							type='work'
 							tech={["html", "css", "js", "php", "pg"]}
 						></TimelineElement>
@@ -81,7 +81,7 @@ function Timeline({ locale }: TimelineProps) {
 							date='Jul 2023 - Aug 2023'
 							type='work'
 							tech={["html", "css", "js", "php", "pg"]}
-							desc='During the second year of my bachelor, I had to complete an internship of 3 months to get a taste of the working world. For this internship, I worked at DATE, a SME specialized in the manufacturing of high-end custom-made heat exchangers for clients in the space, defense or industry sectors. My mission at DATE was to create an application to estimate the cost of a project by taking into account the different expenses ranging from labor to raw materials. I was able to discover the world of industry and business, but also learn to work independently and manage a project from start to finish.'
+							desc='During the second year of my bachelor, I did an internship of 3 months to understand the world of work. For this internship, I worked at DATE, a SME specialized in the manufacturing of high-end custom-made heat exchangers for clients in the space, defense or industry sectors. My mission at DATE was to create an application to estimate the cost of a project by taking into account the different expenses ranging from labor to raw materials. I was able to discover the world of industry and business, but also learn to work independently and manage a project from start to finish.'
 						></TimelineElement>
 					)}
 				</div>
@@ -93,7 +93,7 @@ function Timeline({ locale }: TimelineProps) {
 							color='red'
 							title='Pharmacie de la Commanderie'
 							date='Juil 2023 - Août 2023'
-							desc="Pendant l'été entre ma deuxième et troisème année de BUT, j'ai eu l'opportunité d'utiliser mes compétences acquises en stage dans un milieu complétement nouveau qui est celui de la santé. Pendant un mois, j'ai développé pour une pharmacie un logiciel de gestion de certains éléments administratifs de la pharmacie comme par exemple la gestion de location de matériel, ou encore du stockage et de la recherche de documents administratifs ou de checklists (Voir projet PharmaDesk)."
+							desc="Pendant l'été entre ma deuxième et troisième année de BUT, j'ai eu l'opportunité d'utiliser mes compétences acquises en stage dans le milieu de la santé. Pendant un mois, j'ai développé pour une pharmacie, un logiciel de gestion de certains éléments administratifs clé : gestion de location de matériel, stockage, recherche de documents administratifs et création de checklists (Voir projet PharmaDesk)."
 							type='work'
 							tech={["html", "css", "js", "php", "pg"]}
 						></TimelineElement>
@@ -104,7 +104,7 @@ function Timeline({ locale }: TimelineProps) {
 							date='Jul 2023 - Aug 2023'
 							type='work'
 							tech={["html", "css", "js", "php", "pg"]}
-							desc='After high school, I decided to join La prepa des INP in Nancy with the goal of joining an engineering school from the INP group. During this year, I was able to discover several scientific fields in depth despite difficult working conditions due to the COVID-19 lockdown. I was able to discover computer science which pushed me towards my decision to stop my current formation and go towards computer science studies.'
+							desc="During the summer between my second and third years at of my Bachelor's degree, I had the opportunity to use the skills I acquired during my internship in the healthcare sector. For a month, I developed software for a pharmacy to manage a number of key administrative elements: management of equipment rental, storage, searching for administrative documents and creating checklists (see PharmaDesk project)."
 						></TimelineElement>
 					)}
 				</div>
@@ -118,7 +118,7 @@ function Timeline({ locale }: TimelineProps) {
 							date='Sept 2023 - Juil 2024'
 							type='work'
 							tech={["react", "next", "pg", "html", "css"]}
-							desc="J'ai eu l'occasion de faire ma troisième année de BUT en alternance. Après l'expérience du stage qui s'est bien passée, j'ai décidé, d'un accord commun avec DATE, de faie cette année d'alternance chez eux. Ma mission pour cette année a été dans un premier temps de terminer l'application de chiffrage des projets. Mon deuxième projet était une application permettant de stocker et rechercher des données sur des anciens projets de l'entreprise pour aider lors de la phase d'étude d'un projet. Les employés peuvent aller sur l'application et rechercher en utilisant divers critères techniques ou administratifs pour savoir ce qui a été fait par le passé pour un projet similaire."
+							desc="Après le succès de mon stage chez DATE, j'ai décidé de faie ma troisième année de BUT en alternance chez eux. Ma mission pour cette année a été dans un premier temps de terminer l'application de chiffrage des projets démarée pendant mon stage. Mon deuxième projet avait pour objectif de développer une application permettant de stocker et rechercher des données sur des anciens projets de l'entreprise pour aider lors de la phase d'étude d'un projet afin de générer des devis client constants. Les employés peuvent naviguer l'application et rechercher en utilisant divers critères techniques ou administratifs pour savoir ce qui a été fait par le passé pour un projet similaire."
 						></TimelineElement>
 					) : (
 						<TimelineElement
@@ -127,7 +127,7 @@ function Timeline({ locale }: TimelineProps) {
 							date='Sept 2023 - Juil 2024'
 							type='work'
 							tech={["react", "next", "pg", "html", "css"]}
-							desc="For my bachelor's third year, I had the opportunity to do a work-study program. After the great experience of my internship, I decided, with DATE, to do this work-study program with them to continue my mission. The first thing I had to do was to finish the application I started during my internship. My second project was an application that allows to stored and research data from the company's project in order to help during the study phase of a project. The employees can go to the application and search by using various technical or administrative criteria to find out what was done in the past for a similar project."
+							desc="After the success my internship, I decided to do this work-study program with them. The first thing I had to do was to finish the application I started during my internship. My second project was an application that allows to stored and research data from the company's project in order to help during the study phase of a project to generate consistent estimates. The employees can navigate through the application and search by using various technical or administrative criteria to find out what was done in the past for a similar project."
 						></TimelineElement>
 					)}
 				</div>
--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -21,18 +21,23 @@ export default function Modal({ locale, showModal, setShowModal }: ModalProps) {
 						<div className='relative p-6 flex-auto'>
 							<p className='text-xl text-white font-light mt-5 text-justify'>
 								{locale === "fr"
-									? "Bonjour, je suis Laurent Boualavong, actuellement en 3ème année de BUT informatique à l'IUT2 de Grenoble en alternance. Principalement intéressé par le développement web et logiciel, j'aime pouvoirutiliser mes capacités afin d'assiter au mieux possible dans leur vie. De nature curieux, j'adore apprendre à faire de nouvelles choses."
-									: "Hello, I am Laurent Boualavong, currently in 3rd year of a bachelor's degree in computer science at the IUT2 of Grenoble as a work-study student. Mainly interested in web and software development, I like to use my abilities to assist people as best as possible in their lives. Curious by nature, I love learning new skills."}
+									? "Bonjour, je suis Laurent Boualavong, actuellement en 1ère année aux Mines d'Alès dans la filière informatique et réseaux, parcours développement logiciel en alternance. Principalement intéressé par le développement web et logiciel, j'aime pouvoir utiliser mes capacités afin de trouver des solutions aux problèmes des utilisateurs. De nature curieux, j'adore acquérir de nouvelles connaissances sur divers sujets."
+									: "Hello, I am Laurent Boualavong, currently in 1st year as a work-study student at the engineering school IMT Mines d'Alès. Majoring in computer science and networks, specialized in software development, I am mainly interested in web and software development. I like to use my abilities to create tools or solutions to solve users's problems. Curious by nature, I love learning new skills."}
 							</p>
 							<p className='text-xl text-white font-light mt-3 text-justify'>
 								{locale === "fr"
-									? "Lors du premier confinement de la pandémie de COVID-19, j'ai cherché une manière d'occuper mon temps libre. Étant fan de culture japonaise, je me suis donc lancé dans l'apprentissage du japonais. Lors de mon entrée dans le supérieur j'ai dû mettre en pause mon apprentissage par manque de temps. Lors de mon BUT j'ai eu l'occasion de reprendre le japonais avec des cours à l'université à côté de ma formation pendant trois semestres."
-									: "During the first lockdown of the COVID-19 pandemic, I was looking for a way to spend some of my free time. Being a fan of Japanese culture, I started to learn Japanese on my own. As I finished my High School, I unfortunatly had to stop my learning because I lacked free time. However, during my bachelor's degree, I had the opportunity to take Japanese classes at the university alongside my main classes for three semesters."}
+									? "Lors du premier confinement de la pandémie de COVID-19, étant fan de culture japonaise, je me suis lancé dans l'apprentissage du japonais. Lors de mon BUT j'ai eu l'occasion de reprendre le japonais avec des cours à l'université en parallèle de ma formation."
+									: "During the first lockdown of the COVID-19 pandemic, being a fan of Japanese culture, I started to learn Japanese on my own. During my bachelor's degree, I had the opportunity to take Japanese classes at the university alongside my main classes for three semesters."}
 							</p>
 							<p className='text-xl text-white font-light mt-3 text-justify'>
 								{locale === "fr"
-									? "Passionné de sport, je pratique du volleyball en compétition depuis 5 ans et suis capitaine de mon équipe depuis 4 ans. Avant cela, j'ai fait du tennis en compétition pendant 10 ans. Lorsque je ne fais pas de sport, j'aime cuisiner ou jouer à des jeux vidéo comme Valorant ou Celeste. Par le passé, j'ai aussi fait de la guitare pendant 4 ans."
-									: "Sport lover, I have been playing volleyball copetitively for 5 years and have been team captain for 4 years. Before that, I played tennis competitively for 10 years. When I am not doing sport, I enjoy cooking or playing video games such as Valorant or Celeste. In the past, I played guitar for 4 years."}
+									? "Mes origines multiculturelles et ma curiosité m'ont amené à voyager dans de nombreux pays (Europe, Asie, Amérique du Nord) et ont ouvert mon esprit."
+									: "My multicultural origins and my curiosity brought me to travel to different countries (Europe, Asie, North America) "}
+							</p>
+							<p className='text-xl text-white font-light mt-3 text-justify'>
+								{locale === "fr"
+									? "Passionné de sport, je pratique du volleyball en compétition depuis 5 ans et suis capitaine de mon équipe depuis 4 ans. Avant cela, j'ai fait du tennis en compétition pendant 10 ans. Lorsque je ne fais pas de sport, j'aime cuisiner ou jouer à des jeux vidéo. J'aime écouter de la musique et je joue occasionnement de la guitare et du piano."
+									: "Sport lover, I have been playing volleyball copetitively for 5 years and have been team captain for 4 years. Before that, I played tennis competitively for 10 years. When I am not doing sport, I enjoy cooking or playing video games. I enjoy listening to music and I occasionnaly play guitar and piano."}
 							</p>
 						</div>
 						{/*footer*/}
